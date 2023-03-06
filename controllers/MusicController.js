421df5af4b88f3db0e8c306bb71858cc81d69e72
Nardos <nardossts@gmail.com>
2023-03-06 17:16:38 +0300

    Final Push

--- controllers/MusicController.js
+++ controllers/MusicController.js
@@ -1,22 +1,29 @@
 const Music = require("../models/Music");
 
 const getMusicList = async (req, res) => {
-  const musicList = await Music.find();
-  res.send(musicList);
+  try {
+    const musicList = await Music.find();
+    res.json(musicList);
+  } catch (error) {
+    res.json({ error });
+  }
 };
 
 const addMusic = async (req, res) => {
-  const music = new Music({
-    'title': req.body.title,
-    'artist': req.body.artist,
-    'album': req.body.album,
-    'genre': req.body.genre,
-  });
+  try {
+    const music = new Music({
+      title: req.body.title,
+      artist: req.body.artist,
+      album: req.body.album,
+      genre: req.body.genre,
+    });
 
-  await music.save();
-  res.json(music);
+    await music.save();
+    res.json(music);
+  } catch (error) {
+    res.json({ error });
+  }
 };
-
 const deleteMusic = async (req, res) => {
   const { id } = req.params;
 
@@ -34,80 +41,100 @@ const deleteMusic = async (req, res) => {
 };
 
 const updateMusic = async (req, res) => {
-  Music.findByIdAndUpdate(req.params.id, req.body)
-    .then((updatedSong) => {
-      res.send(updatedSong);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error updating song");
-    });
+  try {
+    Music.findByIdAndUpdate(req.params.id, req.body)
+      .then((updatedSong) => {
+        res.send(updatedSong);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error updating song");
+      });
+  } catch (error) {
+    res.json({ error });
+  }
 };
 
 const getStat = async (req, res) => {
-  const songsCount = await Music.countDocuments();
-  const artistsCount = (await Music.distinct("artist")).length;
-  const albumsCount = (await Music.distinct("album")).length;
-  const genresCount = (await Music.distinct("genre")).length;
+  try {
+    const songsCount = await Music.countDocuments();
+    const artistsCount = (await Music.distinct("artist")).length;
+    const albumsCount = (await Music.distinct("album")).length;
+    const genresCount = (await Music.distinct("genre")).length;
 
-  res.json({
-    songs: songsCount,
-    artists: artistsCount,
-    albums: albumsCount,
-    genres: genresCount,
-  });
+    res.json({
+      songs: songsCount,
+      artists: artistsCount,
+      albums: albumsCount,
+      genres: genresCount,
+    });
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 const getGenre = async (req, res) => {
-  const genres = await Music.aggregate([
-    { $group: { _id: "$genre", count: { $sum: 1 } } },
-    { $project: { _id: 0, genre: "$_id", songs: "$count" } },
-  ]);
+  try {
+    const genres = await Music.aggregate([
+      { $group: { _id: "$genre", count: { $sum: 1 } } },
+      { $project: { _id: 0, genre: "$_id", songs: "$count" } },
+    ]);
 
-  res.json(genres);
+    res.json(genres);
+  } catch (error) {
+    res.json({ error });
+  }
 };
 
 const getArtists = async (req, res) => {
-  const artists = await Music.aggregate([
-    {
-      $group: {
-        _id: "$artist",
-        songs: { $sum: 1 },
-        albums: { $addToSet: "$album" },
+  try {
+    const artists = await Music.aggregate([
+      {
+        $group: {
+          _id: "$artist",
+          songs: { $sum: 1 },
+          albums: { $addToSet: "$album" },
+        },
       },
-    },
-    {
-      $project: {
-        _id: 0,
-        name: "$_id",
-        songs: "$songs",
-        albums: { $size: "$albums" },
+      {
+        $project: {
+          _id: 0,
+          name: "$_id",
+          songs: "$songs",
+          albums: { $size: "$albums" },
+        },
       },
-    },
-  ]);
+    ]);
 
-  res.json(artists);
+    res.json(artists);
+  } catch (error) {
+    res.json({ error });
+  }
 };
 
 const getAlbums = async (req, res) => {
-  const albums = await Music.aggregate([
-    {
-      $group: {
-        _id: { album: "$album", artist: "$artist" },
-        songs: { $sum: 1 },
+  try {
+    const albums = await Music.aggregate([
+      {
+        $group: {
+          _id: { album: "$album", artist: "$artist" },
+          songs: { $sum: 1 },
+        },
       },
-    },
-    {
-      $project: {
-        _id: 0,
-        album: "$_id.album",
-        artist: "$_id.artist",
-        songs: "$songs",
+      {
+        $project: {
+          _id: 0,
+          album: "$_id.album",
+          artist: "$_id.artist",
+          songs: "$songs",
+        },
       },
-    },
-  ]);
+    ]);
 
-  res.json(albums);
+    res.json(albums);
+  } catch (error) {
+    res.json({ error });
+  }
 };
 
 module.exports = {
